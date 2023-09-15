bdf51141894c2226aad281135d9bd6ae212dbb7f
buihongquan <buihongquan@vnpt.vn>
2023-09-15 17:47:50 +0700

    Add temp

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -4,6 +4,9 @@ function RightSidebar() {
       <div className="flex flex-1 flex-col justify-start">
         <h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
       </div>
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+      </div>
     </section>
   )
 }
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { dark } from '@clerk/themes';
+
 function Topbar() {
   const isUserLoggedIn = true;
   return (
@@ -31,6 +33,7 @@ function Topbar() {
         </div>
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
